8188fcb435a81c7fd5f3ddde1799933c94c9ba17
RedXpilot <115593291+RedXpilot@users.noreply.github.com>
2023-03-16 11:07:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+	let strNew = str.toLowerCase();
 	let i=0, j=str.length-1;
 	while(i<j)
 		{
-			if(str[i++]!=str[j--])
+			if(strNew[i++]!=strNew[j--])
 				return false;
 		}
 	return true;
